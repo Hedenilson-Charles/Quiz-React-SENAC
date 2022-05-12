f36c5e432449f462042f799e7e693dc582a4ed8e
Hedenilson-Charles <105455848+Hedenilson-Charles@users.noreply.github.com>
2022-05-12 16:07:21 -0300

    Primeira versão
    Esse é o meu primeiro git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Apresentacao from './Apresentacao';
+import Menu from './Menu';
+import Rodape from './Rodape';
+
+const App = () => {
+
+    require('./App.css');
+
+    const [ tela, alteraTela ] = useState( null );
+
+    if(tela == null) {
+        alteraTela(<Apresentacao alteraTela={alteraTela}/>);
+    }
+
+    
+
+    return(
+
+        <div> 
+            <Menu/>
+
+
+            { tela }
+
+
+
+            <Rodape/>
+        </div>
+
+    );
 }
 
 export default App;
