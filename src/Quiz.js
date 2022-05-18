976dde11908e7a9e0470145b6391a43cd6abbe75
Hedenilson-Charles <105455848+Hedenilson-Charles@users.noreply.github.com>
2022-05-18 15:06:21 -0300

    ds
    Teste

--- src/Quiz.js
+++ src/Quiz.js
@@ -73,70 +73,70 @@ const Quiz = (props) => {
         },
         {
             imagem:"https://tribunapr.uol.com.br/wp-content/uploads/2020/03/03105249/nazare-confusa-970x550.jpg",
-            texto: "",
+            texto: "6 - Em que parte do corpo se encontra a epiglote?",
+            alternativas: ["Estômago","Cabeça","Mãos","Boca"],
+            correta:0,
+
+        },
+        {
+            imagem:"https://i.pinimg.com/236x/fa/53/4e/fa534eb8c3e0cec185c37d1bd4d6ebd8.jpg",
+            texto: "7 - A quantos graus Celsius a água precisa estar para começar a ferver?",
+            alternativas: ["0°","100°","30°","11°"],
+            correta:0,
+
+        },
+        {
+            imagem:"https://thumbs.gfycat.com/PhonyRichHorsefly-mobile.jpg",
+            texto: "8 - Quantos noves tem de 0 a 100?",
+            alternativas: "",
+            correta:0,
+
+        },
+        {
+            imagem:"https://s1.static.brasilescola.uol.com.br/img/2018/03/catedral-de-sao-basilio.jpg",
+            texto: "9 - Qual é a capital da Rússia?",
+            alternativas: "",
+            correta:0,
+
+        },
+        {
+            imagem:"https://imageproxy.ifunny.co/crop:x-20,resize:640x,quality:90x75/images/0bdc40f8a0c0c002f0d5309737803ed26d5e451cca52171843c7d81872a4c3bf_1.jpg",
+            texto: "10 - Em que estado brasileiro aconteceu a guerra de canudos?",
+            alternativas: "",
+            correta:0,
+
+        },
+        {
+            imagem:"https://statig0.akamaized.net/bancodeimagens/cu/fz/2f/cufz2f859fwnmu3m28e09xouc.jpg",
+            texto: "11 - Qual das alternativas contém apenas vacinas contra a covid-19 ?",
+            alternativas: "",
+            correta:0,
+
+        },
+        {
+            imagem: "https://aws1.discourse-cdn.com/nubank/original/3X/4/7/47919cb5977ce20f31e5640ffafb7754583cdb42.jpeg",
+            texto: "12 - O que é Pix?",
             alternativas: "",
             correta:0,
 
         },
         {
             imagem:"",
-            texto: "",
+            texto: "13 - Qual das alternativas contém apenas invenções criadas",
             alternativas: "",
             correta:0,
 
         },
         {
             imagem:"",
-            texto: "",
+            texto: "14 - A condensação é a transição entre quais estados da materia?",
             alternativas: "",
             correta:0,
 
         },
         {
             imagem:"",
-            texto: "",
-            alternativas: "",
-            correta:0,
-
-        },
-        {
-            imagem:"",
-            texto: "",
-            alternativas: "",
-            correta:0,
-
-        },
-        {
-            imagem:"",
-            texto: "",
-            alternativas: "",
-            correta:0,
-
-        },
-        {
-            imagem:"",
-            texto: "",
-            alternativas: "",
-            correta:0,
-
-        },
-        {
-            imagem:"",
-            texto: "",
-            alternativas: "",
-            correta:0,
-
-        },
-        {
-            imagem:"",
-            texto: "",
-            alternativas: "",
-            correta:0,
-
-        },
-        {
-            imagem:"",
-            texto: "",
+            texto: "15 - O Dia Internacional das Mulheres é comemorado em qual mês?",
             alternativas: "",
             correta:0,
 
@@ -144,7 +144,6 @@ const Quiz = (props) => {
 
     ];
 
-    console.log(pontos);
 
     
     return  (
