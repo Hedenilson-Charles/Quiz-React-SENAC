7d9944c9e8b4c7fa4749ee7cb13b8c8094fc4f52
Hedenilson-Charles <105455848+Hedenilson-Charles@users.noreply.github.com>
2022-05-19 15:01:10 -0300

    Versão Final
    (Dessa vez é serio!)

--- src/Apresentacao.js
+++ src/Apresentacao.js
@@ -14,7 +14,7 @@ const Apresentacao = ( props ) => {
     return ( 
     <div className="Apresentacao">
 
-        <h1> Bem-vindo ao quiz ✍🏽 </h1>
+        <h1> Bem-vindo ao  Const Quiz ✍🏽 </h1>
         <p>Aqui você colocará á prova seus conhecimentos gerais</p>
         <div className="Button">
              <button onClick={ ()=> alteraTela( <Quiz alteraTela={alteraTela}/>)<resetaPontos(pontosIniciais = 0)}>Começar</button>
